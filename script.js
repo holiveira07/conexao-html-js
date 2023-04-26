a03725b9326a1fa9d2ac61c52ab31b0d9bff1e4a
holiveira_07 <oliveirasalgadolh@gmail.com>
2023-04-26 18:41:36 -0300

    Atividade e atividade extra da aula ao vivo do dia 24/04 concluída

---
+++ script.js
@@ -0,0 +1,38 @@
+const postagem = document.getElementById("post02");
+ console.log(postagem)
+
+const forms = document.getElementById("formulario");
+ console.log(forms)
+
+const seção = document.getElementById("posts");
+ console.log(seção)
+
+const data = document.getElementsByTagName("p")[1];
+ console.log(data)
+
+ const texto0 = document.getElementsByTagName("p")[2];
+ console.log(texto0.textContent)
+
+const texto1 = document.getElementsByTagName("p")[5];
+ console.log(texto1.textContent)
+
+let link = document.getElementById("linknotexto");
+ console.log(link.innerText)
+
+const palavra = document.getElementById ("palavra1");
+ console.log(palavra.textContent)
+
+const nomeform = document.getElementsByTagName("h2");
+ console.log(nomeform[1].innerText)
+
+const linksredes = document.querySelector("aside");
+ console.log(linksredes.innerHTML)
+
+var navegação = document.getElementById("links_nav");
+  console.log(navegação.innerText)
+
+  var autoresdatas = document.querySelectorAll("p");
+   console.log(autoresdatas [0].textContent)
+   console.log(autoresdatas [1].innerText)
+   console.log(autoresdatas [3].textContent)
+   console.log(autoresdatas [4].textContent)
